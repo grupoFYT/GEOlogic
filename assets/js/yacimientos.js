74a95937736fe2a3e54ab0cb3e009165ca975865
grupoFYT <maxi@mail>
2014-11-04 14:00:01 -0300

    Auto SYNC

--- assets/js/yacimientos.js
+++ assets/js/yacimientos.js
@@ -223,15 +223,12 @@ function drawX() {
 		
 	});
 
-
-	
-	
-
 }
 
 function getInfo() {
 	
 	$('#infoRow h4').html('Zona ' + datax.zona + ' ( Región ' + datax.region + ' )');
+	
 	// var markerBounds = new google.maps.LatLngBounds();
 
 	// $.each(zonasPol , function(index, value) {
@@ -250,65 +247,6 @@ function getInfo() {
 	// });
 }
 
-function drawRegiones() {
-	regionesPol = new Array();
-	zonasPol = new Array();
-	var markerBounds = new google.maps.LatLngBounds();
-	$.each(ZS.regiones , function(index, value) {
-		regionesPol[index] = new Array();
-		regionesPol[index]['coords'] = new Array();
-		$.each(value.coords , function(ix, vx) {
-			regionesPol[index]['coords'].push( new google.maps.LatLng(vx['lat'], vx['lng']) );
-			lat = vx['lat'];
-			Lng = vx['lng'];
-			markerBounds.extend(new google.maps.LatLng(lat, Lng));						
-			map.fitBounds(markerBounds);
-				
-		});
-		regionesPol[index]['zmap'] = new google.maps.Polygon({
-									paths: regionesPol[index]['coords'],
-									draggable: false,
-									editable: false,
-									strokeColor: '#' + value.color,
-									strokeOpacity: 0.8,
-									strokeWeight: 1,
-									fillColor: '#' + value.color ,
-									fillOpacity: 0.3,
-									zIndex: 0
-								});	
-		regionesPol[index]['zmap'].setMap(map);
-		
-		
-		$.each(ZS.zonas , function(indexx, valuex) {
-			if ((valuex.region_id == value.id) && (valuex.coords.length > 0)) {
-				zonasPol[indexx] = new Array();
-				zonasPol[indexx]['id'] = valuex.id;
-				zonasPol[indexx]['name'] = valuex.name;
-				zonasPol[indexx]['coords'] = new Array();
-				$.each(valuex.coords , function(indexxx, valuexx) {
-					zonasPol[indexx]['coords'].push( new google.maps.LatLng(valuexx['lat'], valuexx['lng']) );						
-				});
-				zonasPol[indexx]['zmap'] = new google.maps.Polygon({
-											paths: zonasPol[indexx]['coords'],
-											draggable: false,
-											editable: false,
-											strokeColor: '#ff2012',
-											strokeOpacity: 0.8,
-											strokeWeight: 1,
-											fillColor: '#ff2012',
-											fillOpacity: 0.4,
-											zIndex: 1
-										});	
-				zonasPol[indexx]['zmap'].setMap(map);
-				
-			}
-		});
-			
-		
-	});
-
-
-}
 
 function setTypeahead(objdom, xrhfrunc, dataId, dataProperty, hiddencell) {
 	$(objdom).typeahead({ 
@@ -367,107 +305,3 @@ function setTypeahead(objdom, xrhfrunc, dataId, dataProperty, hiddencell) {
 	});
 }
 
-var minmapOptions = {
-	streetViewControl: false,
-	zoom: 3,
-	center: myLatLng,
-	mapTypeId: google.maps.MapTypeId.HYBRID,
-	disableDefaultUI: true
-};
-
-var minmap;
-
-function drawMinMap() {
-	
-	minmap = new google.maps.Map(document.getElementById('mini-map'),minmapOptions);
-	minmap.setZoom(1);
-	minmap.panTo(myLatLng);
-
-	gotoRegion($('#hiddenRegionID').val());
-
-}
-
-var minZonasPol;
-var minRegionPol;
-
-function gotoRegion(regId) {	
-	
-	if(typeof(xZona) != 'undefined') {
-			xZona.setMap(null);
-	}
-		
-	$.each(ZS.regiones, function(index, value) {		
-		if (value.id == regId) {
-			minZonasPol = null;
-			if(typeof(minRegionPol) != 'undefined') {
-				minRegionPol.setMap(null);
-			}		
-			var regionCoords = [];
-			$.each(value.coords , function(indexx, valuex) {
-				regionCoords.push(new google.maps.LatLng(valuex['lat'], valuex['lng']));				
-			});			
-			minRegionPol = new google.maps.Polygon({
-				paths: regionCoords,
-				draggable: false,
-				editable: false,
-				strokeColor: '#' + value.color,
-				strokeOpacity: 0.8,
-				strokeWeight: 2,
-				fillColor: '#' + value.color ,
-				fillOpacity: 0.3,
-				zIndex: 0
-			});			
-			minRegionPol.setMap(minmap);				
-			var latLng = new google.maps.LatLng(value.c_lat, value.c_lng); //Makes a latlng
-			minmap.setZoom(value.c_zoom);
-			minmap.panTo(latLng);
-
-			minZonasPol = new Array();
-			$.each(ZS.zonas , function(indexx, valuex) {
-				if ((valuex.region_id == value.id) && (valuex.coords.length > 0)) {
-					minZonasPol[indexx] = new Array();
-					minZonasPol[indexx]['coords'] = new Array();
-					$.each(valuex.coords , function(indexxx, valuexx) {
-						minZonasPol[indexx]['coords'].push( new google.maps.LatLng(valuexx['lat'], valuexx['lng']) );						
-					});
-					minZonasPol[indexx]['zmap'] = new google.maps.Polygon({
-												paths: minZonasPol[indexx]['coords'],
-												draggable: false,
-												editable: false,
-												strokeColor: '#ff2012',
-												strokeOpacity: 0.8,
-												strokeWeight: 2,
-												fillColor: '#ff2012',
-												fillOpacity: 0.5,
-												zIndex: 1
-											});	
-					minZonasPol[indexx]['zmap'].setMap(minmap);
-					
-					google.maps.event.addListener(minZonasPol[indexx]['zmap'], 'click', function(event) {
-						
-						var infowindow = new google.maps.InfoWindow();
-						var contentString = '<div id="content" style="width:250px; height: 100px;">'+
-							'Zona ' + valuex.name + '<br>' + 
-							'Area: ' + google.maps.geometry.spherical.computeArea(minZonasPol[indexx]['zmap'].getPath()) + ' mts2' + 							
-						  '</div>';
-
-						infowindow.setContent(contentString);
-						infowindow.setPosition(event.latLng);
-						infowindow.open(minmap);
-					});
-					
-				}
-			});
-			
-			
-			
-		}
-	}); 
-}
-
-function getPolygonCoords() {	
-	var polygonBounds = xZona.getPath();
-	var coordinates = [];
-	for(var i = 0 ; i < polygonBounds.length ; i++) coordinates.push(polygonBounds.getAt(i).lat(), polygonBounds.getAt(i).lng());	
-	$("#coord").val(coordinates);
-}
